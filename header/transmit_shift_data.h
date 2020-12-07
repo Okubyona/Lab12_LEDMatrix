34fffd862ad6ae194535c12ad9911cca58c2a700
Okubyona <andrewbazua@gmail.com>
2020-12-06 17:31:45 -0800

    feat: add support for shift registers on different ports

--- header/transmit_shift_data.h
+++ header/transmit_shift_data.h
@@ -1,9 +1,10 @@
 #ifndef __transmit_shift_data_h__
 #define __transmit_shift_data_h__
 
-// must have <avr/io.h> included
+// must have <avr/io.h> included before this file
 
-void transmit_data(unsigned char data, int val) {
+// For shift registers transmiting from PORTC
+void transmit_dataC(unsigned char data, int val) {
     for (int i = 0; i < 8; ++i) {
             // Sets SRCLR to 1 allowing data to be set
             // Also clears SRCLK in preparation of sending data
@@ -18,6 +19,24 @@ void transmit_data(unsigned char data, int val) {
         PORTC |= 0x04;
         // clears all lines in preparation of new transmission
         PORTC = 0x00;
+}
+
+// For shift registers transmiting from PORTD
+void transmit_dataD(unsigned char data, int val) {
+    for (int i = 0; i < 8; ++i) {
+            // Sets SRCLR to 1 allowing data to be set
+            // Also clears SRCLK in preparation of sending data
+            PORTD = 0x08;
+            // set SER = next bit of data to be sent.
+            PORTD |= ((data >> i) & 0x01);
+            // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+            PORTD |= 0x02;
+
+    }
+        // set RCLK = 1. Rising edge copies data from "Shift" register to "Storage" register
+        PORTD |= 0x04;
+        // clears all lines in preparation of new transmission
+        PORTD = 0x00;
 }
 
 #endif
