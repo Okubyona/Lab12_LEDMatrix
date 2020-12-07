2d0028e342a0860ee2fadd2d3c39b3916b09de36
Okubyona <andrewbazua@gmail.com>
2020-12-06 19:44:04 -0800

    fix: add missing demo link

--- turnin/abazu_lab12_part1.c
+++ turnin/abazu_lab12_part1.c
@@ -7,6 +7,9 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *  Demo Link:
+ *  https://drive.google.com/file/d/1sHmR76C3v8xwuKJUOQ-1Fmf9QSliXlbG/view?usp=sharing
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
